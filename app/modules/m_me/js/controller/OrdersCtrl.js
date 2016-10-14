266630b6b2b1d61c4912bdfd94a04e90c1159e11
AtomicRose <baishenfang@hotmail.com>
2016-10-14 16:25:22 +0800

    no message

---
+++ app/modules/m_me/js/controller/OrdersCtrl.js
@@ -0,0 +1,40 @@
+app.controller('OrdersCtrl',['$scope','$rootScope','$state','$stateParams','UserService','CMSDataConfig','dialog',function($scope,$rootScope,$state,$stateParams,UserService,CMSDataConfig,dialog){
+    window.headerConfig={
+        enableHeader: true,
+        enableBack: true,
+        title: '全部'
+    };
+    var orderStatus = CMSDataConfig.orderStatus;
+    var orderType = $stateParams.orderType;
+    for(var i = 0; i<orderStatus.length; i++){
+        if(orderStatus[i].type == orderType){
+            window.headerConfig.title = orderStatus[i].text;
+        }
+    }
+    $rootScope.$broadcast('setHeaderConfig', window.headerConfig);
+
+    getOrderList(orderType)
+    function getOrderList(orderType){
+        var spinner = dialog.showSpinner();
+        var opt = {
+            bk_status:orderType
+        }
+        UserService.getOrderList(opt).then(
+            function(res){
+                // $scope.orderList
+                $scope.orderList = res.results;
+                dialog.closeSpinner(spinner.id);
+            },
+            function(res){
+                dialog.closeSpinner(spinner.id);
+                dialog.alert(res.errorMsg);
+            }
+        )    
+    }
+
+    $scope.goDetail = function(_id){
+        $state.go('layout.orderDetail',{
+            bookingId: _id
+        })
+    }
+}]);
