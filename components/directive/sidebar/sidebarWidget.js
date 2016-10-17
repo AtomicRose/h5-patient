2141e3132a16bb7eb403b1eafe2efba37cb2b173
AtomicRose <baishenfang@hotmail.com>
2016-10-17 11:54:47 +0800

    add the sidebar

--- components/directive/sidebar/sidebarWidget.js
+++ components/directive/sidebar/sidebarWidget.js
@@ -1,35 +1,55 @@
 app.directive('sidebarWidget', [function () {
-    var ctrl = ['$scope', '$rootScope', 'helper', '$state', function ($scope, $rootScope, helper, $state) {
+    var ctrl = ['$scope', '$rootScope', 'helper', '$state', 'CMSDataConfig', function ($scope, $rootScope, helper, $state, CMSDataConfig) {
         var e_sidebar = document.getElementById('layoutSidebar');
         $scope.isOpen = false;
-        function _open(){
+        function _open() {
             $scope.isOpen = true;
-            if(!$rootScope.$$phase) {
+            if (!$rootScope.$$phase) {
                 $scope.$apply();
             }
             return true;
         }
-        function _close(){
+
+        function _close() {
             $scope.isOpen = false;
-            if(!$rootScope.$$phase) {
+            if (!$rootScope.$$phase) {
                 $scope.$apply();
             }
             return false;
         }
-        function _toggle(){
+
+        function _toggle() {
             $scope.isOpen = !$scope.isOpen;
-            if(!$rootScope.$$phase) {
+            if (!$rootScope.$$phase) {
                 $scope.$apply();
             }
             return $scope.isOpen;
         }
+
         window.LayoutSidebar = {
             open: _open,
             close: _close,
             toggle: _toggle
         };
-        $scope.closeSidebar = function(){
+        $scope.closeSidebar = function () {
             return _close();
+        };
+
+        $scope.menuList = CMSDataConfig.appMenus;
+        $scope.clickItem = function (item) {
+            if (item.beforeCall && typeof(item.beforeCall) === 'function' && item.beforeCall()) {
+                if (item.beforeCall()) {
+                    if (item.route) {
+                        window.LayoutSidebar.close();
+                        $state.go(item.route, item.params);
+                    }
+                }
+            } else {
+                if (item.route) {
+                    window.LayoutSidebar.close();
+                    $state.go(item.route, item.params);
+                }
+            }
         };
     }];
     return {
@@ -47,7 +67,26 @@ app.directive('sidebarWidget', [function () {
 app.run(['$templateCache', function ($templateCache) {
     $templateCache.put('template/sidebar.html',
         '<div class="sidebar" ng-class="{true:\'open\',false:\'close\'}[isOpen]" id="layoutSidebar">\
-            <div class="content"></div>\
+            <div class="content">\
+                <div class="user-info">\
+                    <div class="left">\
+                        <div class="circle">\
+                            <img src="" alt="">\
+                        </div>\
+                    </div>\
+                    <div class="right">您好！请登录</div>\
+                </div>\
+                <div class="cell-group list">\
+                    <div class="cell" ng-repeat="item in menuList" ng-click="clickItem(item)">\
+                        <div class="left-box">\
+                            <i class="iconfont {{item.class}}"></i>\
+                        </div>\
+                        <div class="middle-box">\
+                            <span class="label" ng-bind="item.text"></span>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
             <div class="white" ng-click="closeSidebar()"></div>\
         </div>');
 }]);
--- config/cmsDataConfig/CMSDataConfig.js
+++ config/cmsDataConfig/CMSDataConfig.js
@@ -25,37 +25,34 @@ app.factory('CMSDataConfig', ['StorageConfig', '$state', function (StorageConfig
     
     data.appMenus = [
         {
-            text: '首页',
-            class: 'icon-shouye',
-            route: 'layout.home',
-            url: '/layout/home'
+            text: '我的预约单',
+            class: 'icon-edit',
+            route: 'layout.order'
         },
         {
-            text: '医院',
-            class: 'icon-zantong',
-            route: 'layout.hospital',
-            url: '/layout/hospital'
+            text: '历史浏览',
+            class: 'icon-clock',
+            route: 'history'
         },
         {
-            text: '发现',
-            class: 'icon-xihuan',
-            route: 'layout.find',
-            url: '/layout/find'
+            text: '常见问题',
+            class: 'icon-question',
+            route: 'problem'
         },
         {
-            text: '个人',
-            class: 'icon-wode',
-            route: 'layout.me',
-            url: '/layout/me',
-            beforeCall: function () {
-                if (!StorageConfig.TOKEN_STORAGE.getItem('authorization')) {
-                    $state.go('layout.login',{
-                        redirectRoute: encodeURIComponent('layout.me')
-                    });
-                    return false;
-                }
-                return true;
-            }
+            text: '联系客服',
+            class: 'icon-phone',
+            route: ''
+        },
+        {
+            text: '关于我们',
+            class: 'icon-warning',
+            route: 'about'
+        },
+        {
+            text: '设置',
+            class: 'icon-setting',
+            route: 'setting'
         }
     ];
     return data;
